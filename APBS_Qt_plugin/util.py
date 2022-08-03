664123f9865c394f5ac9359ea0b4de75bc6401f4
Thomas Jackson <tom.jackson314@gmail.com>
2022-08-03 18:05:44 -0400

    Add method to emit all signals from a Model

--- APBS_Qt_plugin/util.py
+++ APBS_Qt_plugin/util.py
@@ -11,11 +11,14 @@ _log = logging.getLogger(__name__)
 
 import pymol.Qt.QtCore as QtCore
 
-
 # ------------------------------------------------------------------------------
 # Qt convenience classes
 
-PYQT_SIGNAL = QtCore.pyqtSignal # name differs between PyQT and PySide
+# name differs between PyQT and PySide; believe pymol.Qt wraps differences for
+# us, but this is just to be safe
+PYQT_SIGNAL = QtCore.pyqtSignal
+PYQT_SLOT = QtCore.pyqtSlot
+PYQT_OBJECT = QtCore.QObject
 
 class SignalWrapper():
     """Descriptor to automatically emit a pyqtSignal (assumed predefined)
@@ -25,12 +28,27 @@ class SignalWrapper():
         self.__set_name__(None, name)
         self.signal_type = signal_type
 
+    @staticmethod
+    def _private_from_public_name(name):
+        return '_' + name
+
+    @staticmethod
+    def _public_from_private_name(name):
+        assert name.startswith('_')
+        return name[1:]
+
+    @staticmethod
+    def _signal_from_public_name(name):
+        return '_' + name + '_changed'
+
     def __set_name__(self, owner, name):
         self.public_name = name
-        self.private_name = '_' + name
-        self.signal_name = '_' + name + '_changed'
+        self.private_name = self._private_from_public_name(name)
+        self.signal_name = self._signal_from_public_name(name)
 
     def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
         return getattr(obj, self.private_name)
 
     def __set__(self, obj, value):
@@ -40,10 +58,22 @@ class SignalWrapper():
         try:
             setattr(obj, self.private_name, value)
             if old_value != value:
-                getattr(obj, self.signal_name).emit(self.signal_type(value))
+                self._emit(obj, value)
         except ValueError:
             # if attrs validation fails, don't emit signal
             raise
+
+    def _emit(self, obj, value=None):
+        """Emit associated signal with current attribute value.
+        """
+        try:
+            if value is None:
+                value = getattr(obj, self.private_name)
+            getattr(obj, self.signal_name).emit(self.signal_type(value))
+        except ValueError:
+            # if attrs validation fails, don't emit signal
+            raise
+
 
 def _attr_field_transformer(cls, fields):
     """Modify field definition process on attrs/dataclass in order to set up
@@ -85,7 +115,7 @@ def attrs_define_w_signals(cls=None, **deco_kwargs):
         return functools.partial(attrs_define_w_signals, **deco_kwargs)
 
     # check that the class we're decorating is capable of emitting Signals
-    assert any(issubclass(cls_, QtCore.QObject) for cls_ in cls.__mro__)
+    assert any(issubclass(cls_, PYQT_OBJECT) for cls_ in cls.__mro__)
 
     # attrs auto-generates an __init__ method; need to manually ensure that
     # super() is called.
@@ -94,15 +124,32 @@ def attrs_define_w_signals(cls=None, **deco_kwargs):
         super(cls, self).__init__()
     setattr(cls, "__attrs_pre_init__", _pre_init)
 
+    # hacky but necessary way to sync up associated Views with the Model. Model
+    # needs to be instantiated before it's connect()ed to views, but this means
+    # views don't know about inital values of model fields. To fix this, provide
+    # a method to manually fire all _*_changed signals for all model fields.
+    def _on_connect(self):
+        cls_ = type(self)
+        for f in attrs.fields(cls_):
+            private_name = f.name # _attr_field_transformer remapped names
+            public_name = SignalWrapper._public_from_private_name(private_name)
+            signal_name = SignalWrapper._signal_from_public_name(public_name)
+            if hasattr(cls_, signal_name):
+                # descriptors are class attributes; call method on the class
+                getattr(cls_, public_name)._emit(self)
+    setattr(cls, "on_connect", _on_connect)
+
+    # apply the attrs dataclass decorator, with descriptors and signals assigned
+    # according to _attr_field_transformer()
     return attrs.define(cls, **deco_kwargs)
 
 @attrs_define_w_signals
-class BaseModel(QtCore.QObject):
+class BaseModel(PYQT_OBJECT):
     """Base class for our Model classes.
     """
     pass
 
-class MultiModel(QtCore.QObject):
+class MultiModel(PYQT_OBJECT):
     """Wrapper for a collection of Models: maintains state of all models, but
     attributes are only looked up on the currently active Model.
     """
