d7b69229e60069618ddd69a8646bae31ff0e26df
Thomas Jackson <tom.jackson314@gmail.com>
2022-08-02 21:40:42 -0400

    Reorganize modules

---
+++ APBS_Qt_plugin/apbs.py
@@ -0,0 +1,300 @@
+"""
+Model, view and controller representing configuration for the APBS code.
+"""
+import dataclasses as dc
+import enum
+import logging
+import pathlib
+import textwrap
+
+_log = logging.getLogger(__name__)
+
+import pymol.Qt.QtCore as QtCore
+import util
+
+if self.apbs_mode.getvalue() == 'Nonlinear Poisson-Boltzmann Equation':
+    apbs_mode = 'npbe'
+else:
+    apbs_mode = 'lpbe'
+
+bcflmap = {'Zero': 'zero',
+            'Single DH sphere': 'sdh',
+            'Multiple DH spheres': 'mdh',
+            #'Focusing': 'focus',
+            }
+bcfl = bcflmap[self.bcfl.getvalue()]
+
+chgmmap = {'Linear': 'spl0',
+            'Cubic B-splines': 'spl2',
+            'Quintic B-splines': 'spl4',
+            }
+chgm = chgmmap[self.chgm.getvalue()]
+
+srfmmap = {'Mol surf for epsilon; inflated VdW for kappa, no smoothing': 'mol',
+            'Same, but with harmonic average smoothing': 'smol',
+            'Cubic spline': 'spl2',
+            'Similar to cubic spline, but with 7th order polynomial': 'spl4', }
+srfm = srfmmap[self.srfm.getvalue()]
+
+@dc.dataclass
+class APBSModelData(util.BaseModelData):
+    """Fields defining config state for options to be passed to APBS.
+    """
+    apbs_path: pathlib.Path
+    apbs_config_file: pathlib.Path
+    apbs_pqr_file: pathlib.Path
+    apbs_dx_file: pathlib.Path
+    apbs_map_name: str
+
+    grid_points: tuple
+    cglen: tuple
+    fglen: tuple
+    cgcent: tuple
+    fgcent: tuple
+    apbs_mode: str = 'Linearized Poisson-Boltzmann Equation'
+    bcfl: str = 'Single DH sphere' # Boundary condition flag
+    ion_plus_one_conc: float = 0.15
+    ion_plus_one_rad: float = 2.0
+    ion_plus_two_conc: float = 0.0
+    ion_plus_two_rad: float = 2.0
+    ion_minus_one_conc: float = 0.15
+    ion_minus_one_rad: float = 1.8
+    ion_minus_two_conc: float = 0.0
+    ion_minus_two_rad: float = 2.0
+    interior_dielectric: float = 2.0
+    solvent_dielectric: float = 78.0
+    chgm: str = 'Cubic B-splines'  # Charge disc method for APBS
+    solvent_radius: float = 1.4
+    system_temp: float = 310.0
+    # sdens: Specify the number of grid points per square-angstrom to use in Vacc
+    # object. Ignored when srad is 0.0 (see srad) or srfm is spl2 (see srfm). There is a direct
+    # correlation between the value used for the Vacc sphere density, the accuracy of the Vacc
+    # object, and the APBS calculation time. APBS default value is 10.0.
+    sdens: float =10.0
+
+        # #"max_mem_allowed" : 400,
+        # "max_mem_allowed": 1500,
+        # "potential_at_sas": 1,
+        # "surface_solvent": 0,
+        # "show_surface_for_scanning": 1,
+
+class APBSModel(
+    QtCore.QObject, APBSModelData, metaclass = util.DataclassDescriptorMetaclass
+):
+    """Config state for options to be passed to APBS.
+    """
+    pass
+
+
+
+class APBSController():
+
+    _file_template = textwrap.dedent("""
+        # Note that most of the comments here were taken from sample
+        # input files that came with APBS.  You can find APBS at
+        # https://github.com/Electrostatics/apbs
+        # Note that APBS is GPL'd code.
+        #
+        read
+            mol pqr {pqr_filename}       # read molecule 1
+        end
+        elec
+            mg-auto
+            # grid calculated by psize.py:
+            dime   {grid_points_0} {grid_points_1} {grid_points_2}  # number of find grid points
+            cglen  {cglen_0} {cglen_1} {cglen_2}                   # coarse mesh lengths (A)
+            fglen  {fglen_0} {fglen_1} {fglen_2}                   # fine mesh lengths (A)
+            cgcent {cent_0} {cent_1} {cent_2}  # (could also give (x,y,z) form psize.py) #known center
+            fgcent {cent_0} {cent_1} {cent_2}  # (could also give (x,y,z) form psize.py) #known center
+            {apbs_mode}     # solve the full nonlinear PBE ("npbe") or linear PBE ("lpbe")
+            bcfl {bcfl}     # Boundary condition flag
+                            #  0 => Zero
+                            #  1 => Single DH sphere
+                            #  2 => Multiple DH spheres
+                            #  4 => Focusing
+
+            #ion 1 0.000 2.0 # Counterion declaration:
+            ion charge  1 conc {ion_plus_one_conc} radius {ion_plus_one_rad}    # Counterion declaration:
+            ion charge -1 conc {ion_minus_one_conc} radius {ion_minus_one_rad}  # ion <charge> <conc (M)> <radius>
+            ion charge  2 conc {ion_plus_two_conc} radius {ion_plus_two_rad}    # ion <charge> <conc (M)> <radius>
+            ion charge -2 conc {ion_minus_two_conc} radius {ion_minus_two_rad}  # ion <charge> <conc (M)> <radius>
+            pdie {interior_dielectric}         # Solute dielectric
+            sdie {solvent_dielectric}          # Solvent dielectric
+            chgm {chgm}     # Charge disc method
+                            # 0 is linear splines
+                            # 1 is cubic b-splines
+            mol 1           # which molecule to use
+            srfm smol       # Surface calculation method
+                            #  0 => Mol surface for epsilon;
+                            #       inflated VdW for kappa; no
+                            #       smoothing
+                            #  1 => As 0 with harmoinc average
+                            #       smoothing
+                            #  2 => Cubic spline
+            srad {solvent_radius} # Solvent radius (1.4 for water)
+            swin 0.3              # Surface cubic spline window .. default 0.3
+            temp {system_temp}    # System temperature (298.15 default)
+            sdens {sdens}         # Specify the number of grid points per square-angstrom to use in Vacc object. Ignored when srad is 0.0 (see srad) or srfm is spl2 (see srfm). There is a direct correlation between the value used for the Vacc sphere density, the accuracy of the Vacc object, and the APBS calculation time. APBS default value is 10.0.
+            #gamma 0.105          # Surface tension parameter for apolar forces (in kJ/mol/A^2)
+                                  # only used for force calculations, so we don't care, but
+                                  # it *used to be* always required, and 0.105 is the default
+            calcenergy no         # Energy I/O to stdout
+                                  #  0 => don't write out energy
+                                  #  1 => write out total energy
+                                  #  2 => write out total energy and all components
+            calcforce no          # Atomic forces I/O (to stdout)
+                                  #  0 => don't write out forces
+                                  #  1 => write out net forces on molecule
+                                  #  2 => write out atom-level forces
+            write pot dx {dx_filename}  # write the potential in dx format to a file.
+        end
+        quit
+
+    """)
+
+    def template_APBS_input_file(pqr_filename,
+                        grid_points,
+                        cglen,
+                        fglen,
+                        cent,
+                        apbs_mode,
+                        bcfl,
+                        ion_plus_one_conc, ion_plus_one_rad,
+                        ion_minus_one_conc, ion_minus_one_rad,
+                        ion_plus_two_conc, ion_plus_two_rad,
+                        ion_minus_two_conc, ion_minus_two_rad,
+                        interior_dielectric, solvent_dielectric,
+                        chgm,
+                        srfm,
+                        solvent_radius,
+                        system_temp,
+                        sdens,
+                        dx_filename,
+                        ):
+        print("Getting APBS input")
+
+
+    def check_input(self):
+        """No silent checks. Always show error.
+        """
+        # First, check to make sure we have valid locations for apbs and psize
+        if not self.binary.valid():
+            raise util.PluginDialogException('Please set the APBS binary location.')
+        # If the path to psize is not correct, that's fine .. we'll
+        # do the calculations ourself.
+
+        # if not self.psize.valid():
+        #    show_error("Please set APBS's psize location")
+        #    return False
+
+        # Now check the temporary filenames
+        if self.radiobuttons.getvalue() != 'Use another PQR':
+            if not self.pymol_generated_pqr_filename.getvalue():
+                raise util.PluginDialogException('Please choose a name for the PyMOL\ngenerated PQR file.')
+        elif not self.pqr_to_use.valid():
+            raise util.PluginDialogException('Please select a valid pqr file or tell\nPyMOL to generate one.')
+        if not self.pymol_generated_pdb_filename.getvalue():
+            raise util.PluginDialogException('Please choose a name for the PyMOL\ngenerated PDB file.')
+        if not self.pymol_generated_dx_filename.getvalue():
+            raise util.PluginDialogException('Please choose a name for the PyMOL\ngenerated DX file.')
+        if not self.pymol_generated_in_filename.getvalue():
+            raise util.PluginDialogException('Please choose a name for the PyMOL\ngenerated APBS input file.')
+        if not self.map.getvalue():
+            raise util.PluginDialogException('Please choose a name for the generated map.')
+
+        # Now, the ions
+        for sign in ('plus', 'minus'):
+            for value in ('one', 'two'):
+                for parm in ('conc', 'rad'):
+                    if not getattr(self, f"ion_{sign}_{value}_{parm}").valid():
+                        raise util.PluginDialogException("Please correct Ion "
+                            f"concentrations and radii (ion_{sign}_{value}_{parm})"
+                        )
+        # Now the grid
+        for grid_type in ('coarse', 'fine', 'points', 'center'):
+            for coord in ('x', 'y', 'z'):
+                if not getattr(self, f"grid_{grid_type}_{coord}").valid():
+                    raise util.PluginDialogException("Please correct grid dimensions"
+                        f"\nby clicking on the "Set grid" button (grid_{grid_type}_{coord})"
+                    )
+
+        # Now other easy things
+        for message, thing in (
+            ('solvent dielectric', self.solvent_dielectric),
+            ('protein dielectric', self.interior_dielectric),
+            ('solvent radius', self.solvent_radius),
+            ('system temperature', self.system_temp),
+            ('sdens', self.sdens),
+        ):
+            if not thing.valid():
+                raise util.PluginDialogException(f"Please correct {message}.")
+
+    def write_APBS_input_file(self):
+        self.check_input()
+
+        # set up our variables
+        pqr_filename = self.getPqrFilename()
+
+        grid_points = [int(getattr(self, 'grid_points_%s' % i).getvalue()) for i in 'x y z'.split()]
+        cglen = [float(getattr(self, 'grid_coarse_%s' % i).getvalue()) for i in 'x y z'.split()]
+        fglen = [float(getattr(self, 'grid_fine_%s' % i).getvalue()) for i in 'x y z'.split()]
+        cent = [float(getattr(self, 'grid_center_%s' % i).getvalue()) for i in 'x y z'.split()]
+
+        if self.apbs_mode.getvalue() == 'Nonlinear Poisson-Boltzmann Equation':
+            apbs_mode = 'npbe'
+        else:
+            apbs_mode = 'lpbe'
+
+        bcflmap = {'Zero': 'zero',
+                    'Single DH sphere': 'sdh',
+                    'Multiple DH spheres': 'mdh',
+                    #'Focusing': 'focus',
+                    }
+        bcfl = bcflmap[self.bcfl.getvalue()]
+
+        chgmmap = {'Linear': 'spl0',
+                    'Cubic B-splines': 'spl2',
+                    'Quintic B-splines': 'spl4',
+                    }
+        chgm = chgmmap[self.chgm.getvalue()]
+
+        srfmmap = {'Mol surf for epsilon; inflated VdW for kappa, no smoothing': 'mol',
+                    'Same, but with harmonic average smoothing': 'smol',
+                    'Cubic spline': 'spl2',
+                    'Similar to cubic spline, but with 7th order polynomial': 'spl4', }
+        srfm = srfmmap[self.srfm.getvalue()]
+
+        dx_filename = self.pymol_generated_dx_filename.getvalue()
+        if dx_filename.endswith('.dx'):
+            dx_filename = dx_filename[:-3]
+        apbs_input_text = template_APBS_input_file(pqr_filename,
+                                            grid_points,
+                                            cglen,
+                                            fglen,
+                                            cent,
+                                            apbs_mode,
+                                            bcfl,
+                                            float(self.ion_plus_one_conc.getvalue()), float(self.ion_plus_one_rad.getvalue()),
+                                            float(self.ion_minus_one_conc.getvalue()), float(self.ion_minus_one_rad.getvalue()),
+                                            float(self.ion_plus_two_conc.getvalue()), float(self.ion_plus_two_rad.getvalue()),
+                                            float(self.ion_minus_two_conc.getvalue()), float(self.ion_minus_two_rad.getvalue()),
+                                            float(self.interior_dielectric.getvalue()), float(self.solvent_dielectric.getvalue()),
+                                            chgm,
+                                            srfm,
+                                            float(self.solvent_radius.getvalue()),
+                                            float(self.system_temp.getvalue()),
+                                            float(self.sdens.getvalue()),
+                                            dx_filename,
+                                            )
+        _log.debug("GOT THE APBS INPUT FILE")
+
+        # write out the input text
+        try:
+            _log.info("Erasing contents of", self.pymol_generated_in_filename.getvalue(), "in order to write new input file")
+            f = open(self.pymol_generated_in_filename.getvalue(), 'w')
+            f.write(apbs_input_text)
+            f.close()
+        except IOError:
+            _log.info("ERROR: Got the input file from APBS, but failed when trying to write to %s" % self.pymol_generated_in_filename.getvalue())
+
+
---
+++ APBS_Qt_plugin/pqr.py
@@ -0,0 +1,316 @@
+"""
+Model, view and controller for PQR file generation configuration.
+"""
+import dataclasses as dc
+import enum
+import logging
+import pathlib
+import shlex
+
+_log = logging.getLogger(__name__)
+
+import pymol.Qt.QtCore as QtCore
+import util
+
+# ----------------------------------
+
+@dc.dataclass
+class BasePQRModelData(util.BaseModelData):
+    """Fields defining config state shared by all PQRModels.
+    """
+    pqr_file: pathlib.Path
+    cleanup_pqr: True
+
+@dc.dataclass
+class PDB2PQRModelData(BasePQRModelData):
+    """Fields defining config state for generating a PQR file using the pdb2pqr
+    binary.
+    """
+    pdb2pqr_path: pathlib.Path
+    pdb_file: pathlib.Path
+class PDB2PQRModel(
+    QtCore.QObject, PDB2PQRModelData, metaclass = util.DataclassDescriptorMetaclass
+):
+    """Config state for generating a PQR file using the pdb2pqr binary.
+    """
+    pass
+
+@dc.dataclass
+class PyMOLPQRExistingHModelData(BasePQRModelData):
+    """Fields defining config state for generating a PQR file using PyMol with
+    existing hydrogens and termini.
+    """
+    pass
+class PyMOLPQRExistingHModel(
+    QtCore.QObject, PyMOLPQRExistingHModelData, metaclass = util.DataclassDescriptorMetaclass
+):
+    """Config state for generating a PQR file using PyMol with existing hydrogens
+    and termini.
+    """
+    pass
+
+@dc.dataclass
+class PyMOLPQRAddHModelData(BasePQRModelData):
+    """Fields defining config state for generating a PQR file using PyMol with
+    PyMol-added hydrogens and termini.
+    """
+    pass
+class PyMOLPQRAddHModel(
+    QtCore.QObject, PyMOLPQRAddHModelData, metaclass = util.DataclassDescriptorMetaclass
+):
+    """Config state for generating a PQR file using PyMol with PyMol-added hydrogens
+    and termini.
+    """
+    pass
+
+@dc.dataclass
+class PreExistingPQRModelData(BasePQRModelData):
+    """Fields defining config state for using a pre-existing PQR file.
+    """
+    pass
+class PreExistingPQRModel(
+    QtCore.QObject, PreExistingPQRModelData, metaclass = util.DataclassDescriptorMetaclass
+):
+    """Config state for using a pre-existing PQR file.
+    """
+    pass
+
+class MultiPQRModel(QtCore.QObject):
+    """Wrapper for all PQRModel states, and user selection of PQR file generation
+    method.
+    """
+    _pqr_multimodel_index_changed = QtCore.pyqtSignal(int)
+
+    def __init__(self):
+        self.pqr_multimodel_index = util.SignalWrapper("pqr_multimodel_index", default=0)
+        self._model_state = (
+            PDB2PQRModel(),
+            PyMOLPQRExistingHModel(),
+            PyMOLPQRAddHModel(),
+            PreExistingPQRModel()
+        )
+
+    def __getattr__(self, name):
+        """Pass through all attribute access to the currently selected PQRModel.
+        """
+        return getattr(self._model_state[self.pqr_multimodel_index], name)
+
+    def __setattr__(self, name, value):
+        """Pass through all attribute access to the currently selected PQRModel.
+        """
+        setattr(self._model_state[self.pqr_multimodel_index], name, value)
+
+# ----------------------------------
+
+
+class BasePQRController():
+    """Base class with common methods for all implementations of PQR file generation.
+    """
+    def fix_columns(self, sel):
+        """
+        Make sure that everything fits into the correct columns.
+        This means doing some rounding. It also means getting rid of
+        chain, occupancy and b-factor information.
+        """
+        pymol.cmd.alter_state(1, 'all', '(x,y,z)=float("%.3f"%x),float("%.3f"%y),float("%.3f"%z)')
+        pymol.cmd.alter(sel, 'chain=""')
+        pymol.cmd.alter(sel, 'b=0')
+        pymol.cmd.alter(sel, 'q=0')
+
+    def cleanup_generated_file(self, filename):
+        """
+        More cleanup on PQR files.
+
+        pdb2pqr will happily write out a file where the coordinate
+        columns overlap if you have -100.something as one of the
+        coordinates, like
+
+        90.350  97.230-100.010
+
+        and so will PyMOL. We can't just assume that it's 0-1
+        because pdb2pqr will debump things and write them out with
+        3 digits post-decimal.
+        """
+        f = open(filename, 'r')
+        txt = f.read()
+        f.close()
+        _log.info(f"Erasing contents of {filename} in order to clean it up")
+        f = open(filename, 'w')
+        # APBS accepts whitespace-delimited columns
+        coordregex = r'([- 0-9]{4}\.[ 0-9]{3})'
+        txt = re.sub(
+                r'^(ATOM  |HETATM)(........................)' + 3 * coordregex,
+                r'\1\2 \3 \4 \5', txt, flags=re.M)
+        f.write(txt)
+        f.close()
+
+class PreExistingPQRController(BasePQRController):
+    """Logic for using a pre-existing PQR file.
+    """
+    pass
+
+class PDB2PQRController(BasePQRController):
+    """Logic for generating a PQR file using the pdb2pqr binary.
+    """
+    def get_unassigned_atoms(self, fname):
+        """
+        Return string of unassigned atoms, identified via warning text in comments
+        in output.
+        """
+        f = open(fname)
+        # Text contains PQR output string
+        unassigned = re.compile('REMARK   5 *(\d+) \w* in').findall(f.read())
+        f.close()
+        return '+'.join(unassigned)
+
+    # PQR generation routines are required to call
+    # cleanup_generated_file themselves.
+    def _write_pdb2pqr_file(self):
+        """Use pdb2pqr to generate a PQR file.
+        Call this via the wrapper write_PQR_file()
+        """
+        # First, generate a PDB file
+        pdb_filename = self.pymol_generated_pdb_filename.getvalue()
+        try:
+            _log.info(f"Erasing contents of {pdb_filename} in order to generate new PDB file")
+            f = open(pdb_filename, 'w')
+            f.close()
+        except:
+            raise util.PluginDialogException("Please set a temporary PDB file location "
+                "that you have permission to edit")
+
+        sel = "((%s) or (neighbor (%s) and hydro))" % (
+            self.selection.getvalue(), self.selection.getvalue())
+
+        apbs_clone = pymol.cmd.get_unused_name()
+        pymol.cmd.create(apbs_clone,sel)
+        self.fix_columns(apbs_clone)
+        pymol.cmd.save(pdb_filename, apbs_clone)
+        pymol.cmd.delete(apbs_clone)
+
+        # Now, generate a PQR file
+        args = [self.pdb2pqr.getvalue(),
+                ] + shlex.split(self.pdb2pqr_options.getvalue()) + [
+                            pdb_filename,
+                            self.pymol_generated_pqr_filename.getvalue(),
+        ]
+        try:
+            args = ' '.join(map(str, args))
+            _log.info("args are now converted to string: ", args)
+#                retval = main.mainCommand(args)
+            if 'PYMOL_GIT_MOD' in os.environ:
+                os.environ['PYTHONPATH'] = os.path.join(os.environ['PYMOL_GIT_MOD']) + ":" + os.path.join(os.environ['PYMOL_GIT_MOD'], "pdb2pqr")
+            pymol_env = os.environ
+            callfunc = subprocess.Popen(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=pymol_env)
+            child_stdout, child_stderr = callfunc.communicate()
+            _log.info(child_stdout)
+            _log.info(child_stderr)
+            retval = callfunc.returncode
+            _log.info("PDB2PQR's mainCommand returned", retval)
+        except Exception as exc:
+            _log.warning("Exception raised by main.mainCommand!")
+            _log.warning(sys.exc_info())
+            retval = 1
+
+        if retval != 0:
+            raise util.PluginDialogException(f"Could not run pdb2pqr: {self.pdb2pqr.getvalue()} "
+                f"{args}\n\nIt returned {retval}.\nCheck the PyMOL external GUI window "
+                "for more information.\n"
+            )
+        self.cleanup_generated_file(self.pymol_generated_pqr_filename.getvalue())
+        unassigned_atoms = self.get_unassigned_atoms(self.pymol_generated_pqr_filename.getvalue())
+        if unassigned_atoms:
+            pymol.cmd.select('unassigned', f"ID {unassigned_atoms}")
+            _log.warning(f"Unassigned atom IDs: {unassigned_atoms}")
+            raise util.PluginDialogException(f"Unable to assign parameters for the "
+                f"{len(unassigned_atoms.split('+'))} atoms in selection 'unassigned'.\n"
+                "Please either remove these unassigned atoms and re-start the calculation\n"
+                "or fix their parameters in the generated PQR file and run the calculation\n"
+                "using the modified PQR file (select 'Use another PQR' in 'Main')."
+            )
+        _log.debug("I WILL RETURN TRUE from pdb2pqr")
+
+    # PQR generation routines are required to call
+    # cleanup_generated_file themselves.
+    def _write_pymol_pqr_file(self):
+        """Generate a pqr file from pymol.
+
+        This will also call through to champ to set the Hydrogens and charges
+        if it needs to.  If it does that, it may change the value self.selection
+        to take the new Hydrogens into account.
+
+        To make it worse, APBS seems to freak out when there are chain ids.  So,
+        this gets rid of the chain ids.
+
+        call this through the wrapper write_PQR_file
+        """
+        try:
+            from chempy.champ import assign
+        except ModuleNotFoundError:
+            raise util.PluginDialogException("_write_pymol_pqr_file couldn't import chempy.champ.")
+
+        # CHAMP will break in many cases if retain_order is set. So,
+        # we unset it here and reset it later. Note that it's fine to
+        # reset it before things are written out.
+        ret_order = pymol.cmd.get('retain_order')
+        pymol.cmd.set('retain_order', 0)
+
+        sel = self.selection.getvalue()
+        sel = f"(({sel}) or (neighbor ({sel}) and hydro))"
+        pqr_filename = self.getPqrFilename()
+        try:
+            _log.debug(f"Erasing previous contents of {pqr_filename}")
+            f = open(pqr_filename, 'w')
+            f.close()
+        except Exception as exc:
+            raise util.PluginDialogException("Could not write PQR file.\n"
+                f"Please check that temporary PQR filename \"{pqr_filename}\" is valid."
+            )
+
+        # PyMOL + champ == pqr
+        if self.radiobuttons.getvalue() == 'Use PyMOL generated PQR and PyMOL generated Hydrogens and termini':
+            pymol.cmd.remove('hydro and %s' % sel)
+            assign.missing_c_termini(sel)
+            assign.formal_charges(sel)
+            pymol.cmd.h_add(sel)
+
+        assign.amber99(sel)
+        pymol.cmd.set('retain_order', ret_order)
+
+        apbs_clone = pymol.cmd.get_unused_name()
+        pymol.cmd.create(apbs_clone, sel)
+        self.fix_columns(apbs_clone)
+        pymol.cmd.save(pqr_filename, apbs_clone)
+        pymol.cmd.delete(apbs_clone)
+
+        self.cleanup_generated_file(pqr_filename)
+        missed_count = pymol.cmd.count_atoms(f"({sel}) and flag 23")
+        if missed_count > 0:
+            pymol.cmd.select("unassigned", f"({sel}) and flag 23")
+            raise util.PluginDialogException(f"Unable to assign parameters for the {missed_count} "
+                "atoms in selection 'unassigned'.\nPlease either remove these unassigned atoms "
+                "and re-start the calculation\nor fix their parameters in the generated PQR file "
+                "and run the calculation\nusing the modified PQR file (select 'Use another PQR' in 'Main')."
+            )
+
+    def write_PQR_file(self):
+        ''' Wrapper for all of our PQR generation routines.
+
+        - Generate PQR file if necessary
+        - Return False if unsuccessful, True if successful.
+
+        - Clean up PQR file if we generated it (the
+          generate... functions are required to do this.).
+        '''
+        if self.radiobuttons.getvalue() == 'Use another PQR':
+            pass
+        elif self.radiobuttons.getvalue() == 'Use PDB2PQR':
+            _log.debug("GENERATING PQR FILE via PDB2PQR")
+            self._write_pdb2pqr_file()
+        else:
+            # it's one of the pymol-generated options
+            _log.debug("GENERATING PQR FILE via PyMOL")
+            self._write_pymol_pqr_file()
+        _log.debug("GENERATED")
+
+# ------------------------------------------------------------------------------
