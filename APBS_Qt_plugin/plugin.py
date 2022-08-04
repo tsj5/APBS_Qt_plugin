cbc65b9289332e9c76549785bf8562c79cba3687
Thomas Jackson <tom.jackson314@gmail.com>
2022-08-04 16:18:05 -0400

    Initial commit plugin.py

---
+++ APBS_Qt_plugin/plugin.py
@@ -0,0 +1,25 @@
+"""
+Top-level plugin state and logic.
+"""
+import logging
+_log = logging.getLogger(__name__)
+
+import util
+
+# ------------------------------------------------------------------------------
+# Models
+
+class PluginModel(util.BaseModel):
+    pass
+
+# ------------------------------------------------------------------------------
+# Controllers
+
+class PluginController(util.BaseController):
+    pass
+
+# ------------------------------------------------------------------------------
+# Views
+
+class PluginView():
+    pass
--- __init__.py
+++ __init__.py
@@ -13,3 +13,35 @@ def __init_plugin__(app=None):
     except ModuleNotFoundError:
         print("APBS_Qt_Plugin: couldn't import pymol. Aborting.")
     addmenuitemqt('APBS Tools...', run_plugin_gui)
+
+# global reference to avoid garbage collection of our dialog
+PLUGIN_DIALOG = None
+
+def run_plugin_gui():
+    '''Open our custom dialog.
+    '''
+    import APBS_Qt_plugin.util as util
+    global PLUGIN_DIALOG
+
+    # set up logging: print any messages to PyMol terminal
+    import logging
+    _log = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.DEBUG)
+
+    try:
+        if PLUGIN_DIALOG is None:
+            PLUGIN_DIALOG = self.view()
+        PLUGIN_DIALOG.show()
+
+    # handle uncaught exceptions
+    except util.PluginDialogException as exc:
+        import sys, traceback
+        from pymol.Qt import QtWidgets
+
+        QMB = QtWidgets.QMessageBox
+        parent = QtWidgets.QApplication.focusWidget()
+        msg = str(exc) or 'unknown error'
+        msgbox = QMB(QMB.Critical, 'Error', msg, QMB.Close, parent)
+        _, _, tb = sys.exc_info()
+        msgbox.setDetailedText(''.join(traceback.format_tb(tb)))
+        msgbox.exec_()
